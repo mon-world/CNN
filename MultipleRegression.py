9e4228085a75685ecb1e8024f1676bd898efd9bd
YANG_MON <audgns346712@gmail.com>
2021-12-21 12:51:18 +0900

    2021 12 21 project add new file

--- MultipleRegression.py
+++ MultipleRegression.py
@@ -1,27 +1,7 @@
-# Day_01_01_MultipleRegression.py
+
 import tensorflow.keras as keras
 import pandas as pd
 import numpy as np
-
-# scikit-learn, landas
-
-# tensorflow
-
-# 1. 파이참 프로젝트 생성
-#    프로젝트 이름은 CNN
-# 2. 프로젝트에 파일 추가
-#    파일 이름은 Day_01_01_MultipleRegression.py
-
-# 파일 실행 ctrl + shift + f10
-# alt + 1 : project 여닫기
-# alt + 4 : run 여닫기
-# ctrl + c + v + v : 한 줄 복사
-# ctrl + x : 오려두기
-# tap / shift + tap : 들여쓰기 / 내어쓰기
-# ctrl + / : 주석
-
-# cudart64 : gpu 버전 설치했다는 뜻.
-# ctrl + 마우스 클릭 : 함수에 대한 설명
 
 def linear_regression():
     x = [1,2,3]
@@ -66,16 +46,15 @@ def multiple_regression():  # 피쳐가 여러개 있는 경우.
 
     model.fit(x,y, epochs=100, verbose= 2)      # 0(없음) 1(전체) 2(약식)
 
-    # 퀴즈
     # 3시간 공부하고 8번 출석한 학생과
-    # 6시간 공부하고 2번 출석한 학생의 성적을 구하시오.
+    # 6시간 공부하고 2번 출석한 학생의 성적을 구하기
     print(model.predict([[3,8],[6,2]]))
 
 
 def multiple_regression_trees():
     trees = pd.read_csv("data/trees.csv")
     print(trees)
-    print(trees.values)     # numpy로 변경됨
+    print(trees.values)
     x = trees.values[:, :-1]
     y = trees.values[:, -1:]
     print(x.shape, y.shape)
